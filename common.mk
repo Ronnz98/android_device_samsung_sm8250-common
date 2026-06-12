db29ccd322ab1068aa91c143707a3b7984d7930e
Ronnz98 <56398486+Ronnz98@users.noreply.github.com>
2026-06-12 18:51:28 +0200

    Update common.mk
    fix

--- common.mk
+++ common.mk
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+COMMON_PATH := device/samsung/sm8250-common
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
